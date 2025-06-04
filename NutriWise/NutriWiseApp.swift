0082e10077f64292444f19eaa5212566c5fde532
Yasin Cetin <yasincetin2014@gmail.com>
2025-06-04 14:14:14 +0300

    initial commit

---
+++ NutriWise/NutriWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriWiseApp.swift
+//  NutriWise
+//
+//  Created by Yasin Cetin on 4.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NutriWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
